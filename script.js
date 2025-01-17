a1925a670a1efd4b9c270689f6907cfd4a2e0a1c
Yuriy Romanchuk <romanchuk.yura@gmail.com>
2025-01-17 10:50:16 +0200

    test

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Привіт");
+// console.log("Привіт");
+
+
+const greeting = "Привіт ";
+const user = "Саша";
+const lowGreeting = greeting.toLowerCase;
+const upperUser = user.toUpperCase;
+console.log(lowGreeting);
+
+
+// console.log("Привіт", "Саша");
+
+
+// const text = "Привіт, мої любі друзі";
+// const dov = text.length;
+// console.log(dov);
+
+// alert("hi");
