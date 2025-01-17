184f0a13714bfe17693cf8fda0ba53c9a0d3bd8a
Yuriy Romanchuk <romanchuk.yura@gmail.com>
2025-01-17 12:11:40 +0200

    as

--- file_system.js
+++ file_system.js
@@ -1,7 +1,18 @@
-const { error } = require("console");
 const fs = require("fs");
+const error = require("console");
 
-fs.readFile("Readme.txt", "utf-8", (error, datas) => {
-    if (error) throw error;
-    console.log(data);
+// const { error } = require("console");
+// const fs = require("fs");
+
+// fs.readFile("Readme.txt", "utf-8", (error, data) => {
+//     if (error) throw error;
+//     console.log(data);
+// });
+
+fs.writeFile("delete_me.txt", "Some text for texting", (error) => {
+    if (error) {
+    console.log("Can not create a new file") }
+    else { 
+        console.log("Create succeful") 
+    }
 });
